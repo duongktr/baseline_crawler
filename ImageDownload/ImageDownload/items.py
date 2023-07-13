63a644ca84ccc8b23242a544d71c2382dc7f2d42
duong233 <bigboy2332k@gmail,com>
2023-07-13 11:20:37 +0700

    update second time

--- ImageDownload/ImageDownload/items.py
+++ ImageDownload/ImageDownload/items.py
@@ -10,5 +10,12 @@ from pprint import pformat
 class ImageDownloadItem(scrapy.Item):
     # define the fields for your item here like:
     url = Field()
-    author_name = Field()
+    author = Field()
     size = Field()
+
+    def __str__(self) -> str:
+        return pformat({
+            'image_url': self['url'],
+            'image_author': self['author'],
+            'size': self['size']
+        })
--- ImageDownload/ImageDownload/spiders/images.py
+++ ImageDownload/ImageDownload/spiders/images.py
@@ -1,44 +1,49 @@
 import scrapy
 from items import ImageDownloadItem
-import os
+import pandas as pd
 from selenium import webdriver
 
 class ImageSpider(scrapy.Spider):
 
     name = 'ImageSpider'
 
+    #file urls.csv include 'url' column containing url, 'domain' column containing domain
+    #read csv file to dataframe
+    df = pd.read_csv('urls.csv', usecols=['url', 'domain']).dropna().reset_index()
+
     def __init__(self, name=None, **kwargs):
         super(ImageSpider, self).__init__(name, **kwargs)
-        self.start_urls = ['https://www.flickr.com/photos/ryanlenguyen'] #example: url get image
-        self.allowed_domains = ['www.flickr.com']
+        #read dataframe to list urls
+        self.start_urls = self.df['url'].values.tolist() 
+        #read dataframe to list domains
+        self.allowed_domains = self.df['domain'].values.tolist()
 
     def start_requests(self):
         for url in self.start_urls:
             yield scrapy.Request(url=url, callback=self.parse)
     
     def parse(self, response):
+
         #config selenium chrome
         options = webdriver.ChromeOptions()
         options.add_argument("headless")
         desired_capabilities = options.to_capabilities()
         driver = webdriver.Chrome(desired_capabilities=desired_capabilities)
-
         #scroll to the end of page
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") #prevent lazy loading
         
-        #get url image
-        for photo in response.css('img').getall():
+        #get url image in flickr website
+        for photo in response.css('img'):
             photo_url = format_url(photo.attrib["src"])
-            yield {
-                "url": photo_url
-            }
+            yield scrapy.Request(photo_url, self.parse_image)
+
         driver.quit()
-    
-    def parse_images(self, response):
-        item = ImageDownloadItem()
-        yield item
+   
+    def parse_image(self, response):
+        return ImageDownloadItem(url=response.photo_url)
         
 
+#format url for flickr
 def photo_url_flickr(photo):
     return 'https://live.staticflickr.com/{server}/{id}_{secret}_{size}.jpg'.format(
         server=photo.xpath('@server').extract_first(),
@@ -47,5 +52,6 @@ def photo_url_flickr(photo):
         size='b',
     )
 
+#format
 def format_url(url):
     return 'https:'+url
